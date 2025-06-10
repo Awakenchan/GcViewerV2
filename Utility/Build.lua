f337b516c9d393b7621591755ae8484753bf60e4
Awaken <93601997+Awakenchan@users.noreply.github.com>
2025-06-10 20:33:10 +0500

    Update Build.lua

--- Utility/Build.lua
+++ Utility/Build.lua
@@ -1,11 +1,14 @@
 return setmetatable({}, {
     __index = function(_, key)
         if key == "__version" then
-            return "2.8"
+            return "3"
         elseif key == "__log" then
             return {
-                "[+] Fixed potassium issue",
-                "[/] Added my crypto address if yall wana support!"
+                "[+] Ported table serialization to Data2Code",
+                "[+] Fixed github urls not having IDE",
+                "[+] Added Name = Target name support when using filtergc",
+                "[/] Fixed genscript fromat for env table",
+                "[/] Made it 2 gui module (was 3 before not including console)"
             }
         end
     end
