17d3faafa2e3bc51ed6f68022cf2d6c9c4fa2c56
Awaken <93601997+Awakenchan@users.noreply.github.com>
2025-06-12 20:09:16 +0500

    Update Build.lua

--- Utility/Build.lua
+++ Utility/Build.lua
@@ -1,14 +1,11 @@
 return setmetatable({}, {
     __index = function(_, key)
         if key == "__version" then
-            return "3"
+            return "3.1"
         elseif key == "__log" then
             return {
-                "[+] Ported table serialization to Data2Code",
-                "[+] Fixed github urls not having IDE",
-                "[+] Added Name = Target name support when using filtergc",
-                "[/] Fixed genscript fromat for env table",
-                "[/] Made it 2 gui module (was 3 before not including console)"
+                "[/] Fixed erlc crashes",
+                "[/] Should be ud on all games."
             }
         end
     end
