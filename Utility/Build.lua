90d851d3068698497e20569701acd1ecf52c3d98
Awaken <93601997+Awakenchan@users.noreply.github.com>
2025-06-29 14:13:07 +0500

    Update Build.lua

--- Utility/Build.lua
+++ Utility/Build.lua
@@ -1,11 +1,14 @@
 return setmetatable({}, {
     __index = function(_, key)
         if key == "__version" then
-            return "3.6"
+            return "4.0"
         elseif key == "__log" then
             return {
                 "[+] Added Dump all functions Const+Upval",
-                "[+] Fixed overview label error"
+                "[+] Fixed overview label error",
+                "[+] Added Search Filters",
+                "[+] Added Search bar",
+                "[/] Improved decompile logic slightly"
             }
         end
     end
