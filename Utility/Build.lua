8dd6a3fcf51b6cd846dd12ce7109c160b9363f33
Awaken <93601997+Awakenchan@users.noreply.github.com>
2025-07-01 15:08:55 +0500

    Updated Build.lua via Discord by awakenkn

--- Utility/Build.lua
+++ Utility/Build.lua
@@ -1,1 +1,15 @@
-return setmetatable({}, {     index = function(_, key)         if key == "version" then             return "4.1"         elseif key == "__log" then             return {                 "[+] Added Dump all functions Const+Upval",                 "[+] Fixed overview label error",                 "[+] Added Search Filters",                 "[+] Added Search bar",                 "[/] Improved decompile logic slightly"             }         end     end })
+return setmetatable({}, {
+    __index = function(_, key)
+        if key == "__version" then
+            return "4.0"
+        elseif key == "__log" then
+            return {
+                "[+] Added Dump all functions Const+Upval",
+                "[+] Fixed overview label error",
+                "[+] Added Search Filters",
+                "[+] Added Search bar",
+                "[/] Improved decompile logic slightly"
+            }
+        end
+    end
+})
