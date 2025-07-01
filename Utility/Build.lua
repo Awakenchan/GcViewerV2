cf1b8b36d9826c50aa21e153e6f0415ea73eddb1
Awaken <93601997+Awakenchan@users.noreply.github.com>
2025-07-01 22:37:49 +0500

    Update Build.lua

--- Utility/Build.lua
+++ Utility/Build.lua
@@ -7,7 +7,7 @@ return setmetatable({}, {
                 "[+] Added Filtering functions via Max constants/upvalue.",
                 "[+] Added Multi Auto-Load-Config support.",
                 "[+] Added CodeEditor for function constants and upvalues to prevent lag.",
-                "[+] Added More do detectfunc > now show more label pops to prevent mass loading.",
+                "[+] Added More to detectfunc > now shows more <label Press able> to prevent mass loading.",
                 "[/] Fixed call back errors on grab func.",
                 "[/] Improved decompile file again.",
                 "[/] Error handler on other stuff."
