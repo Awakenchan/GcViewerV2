a88d10ca762d08ef8b0eb3be323e8063bafcbf4c
Awaken <93601997+Awakenchan@users.noreply.github.com>
2025-07-03 18:17:47 +0500

    Updated Build.lua via Discord by awakenkn

--- Utility/Build.lua
+++ Utility/Build.lua
@@ -1,16 +1,12 @@
 return setmetatable({}, {
     __index = function(_, key)
         if key == "__version" then
-            return "4.2"
+            return "4.3"
         elseif key == "__log" then
             return {
-                "[+] Added Filtering functions via Max constants/upvalue.",
-                "[+] Added Multi Auto-Load-Config support.",
-                "[+] Added CodeEditor for function constants and upvalues to prevent lag.",
-                "[+] Added More to detectfunc > now shows more <label Press able> to prevent mass loading.",
-                "[/] Fixed call back errors on grab func.",
-                "[/] Improved decompile file again.",
-                "[/] Error handler on other stuff."
+                "[+] Added Hide old function labels",
+                "[/] Fixed decompile crashes",
+                "[/] Fixed frequent crashes with decompile"
             }
         end
         return nil 
