2bc9516aff420a94a7f910a9d7eb541bd21ccdae
Awaken <93601997+Awakenchan@users.noreply.github.com>
2025-10-20 06:47:30 +0500

    Update Build.lua

--- Utility/Build.lua
+++ Utility/Build.lua
@@ -1,10 +1,11 @@
 return setmetatable({}, {
     __index = function(_, key)
         if key == "__version" then
-            return "5.4"
+            return "5.4.1"
         elseif key == "__log" then
             return {
-                    "[/] Detection patching"
+                    "[/] Detection patching",
+                    "[/] MetaMethod invoking patched",
             }
         end
         return nil 
