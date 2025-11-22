ca829226c660385760bfb27dd27a5953e6a25bda
Awaken <93601997+Awakenchan@users.noreply.github.com>
2025-11-22 05:45:05 +0500

    Update Build.lua

--- Utility/Build.lua
+++ Utility/Build.lua
@@ -1,14 +1,10 @@
 return setmetatable({}, {
     __index = function(_, key)
         if key == "__version" then
-            return "5.5"
+            return "5.6"
         elseif key == "__log" then
             return {
-                "[+] AssetDownloader and checker",
-                "[/] Name abuse fixes",
-                "[/] Gui detection mitigation",
-                "[+] Added safeScriptInfo function",
-                "[/] Various optimizations and bug fixes"
+                "[+] Table values/table dumping"
             }
         end
         return nil 
