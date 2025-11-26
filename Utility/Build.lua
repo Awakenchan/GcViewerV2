978d73d6ba926bc3bd22852c3a946691933a4079
Awaken <93601997+Awakenchan@users.noreply.github.com>
2025-11-26 21:07:19 +0500

    Update Build.lua

--- Utility/Build.lua
+++ Utility/Build.lua
@@ -1,10 +1,10 @@
 return setmetatable({}, {
     __index = function(_, key)
         if key == "__version" then
-            return "5.6"
+            return "5.7"
         elseif key == "__log" then
             return {
-                "[+] Table values/table dumping"
+                "[/] GC> improved Table values/table dumping"
             }
         end
         return nil 
